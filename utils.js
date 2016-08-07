0d30fe125516459534088f20a50299b196066ffe
Michael Shakmakov <Michael_Shakmakov@epam.com>
2016-08-07 22:22:07 +0400

    dates api for diary

--- utils.js
+++ utils.js
@@ -1,7 +1,8 @@
 module.exports = {
   compareDates: function(oneDate, anotherDate, dayOffset) {
+    console.log()
     return oneDate.getFullYear() === anotherDate.getFullYear() &&
       oneDate.getMonth() === anotherDate.getMonth() &&
-      oneDate.getDate() === anotherDate.getDate() - dayOffset || 0
+      oneDate.getDate() === anotherDate.getDate() - (dayOffset || 0)
   }
 };
